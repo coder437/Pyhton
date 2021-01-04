dfef7c82b8471562012ee55a1f0250d9d90de0b8
coder437 <65409756+coder437@users.noreply.github.com>
2021-01-04 16:53:43 +0530

    Add files via upload

---
+++ guessTheNumber.py
@@ -0,0 +1,14 @@
+import random
+print("Guess a number between 1 to 10")
+MIN = 1
+MAX = 10
+
+targetNumber = random.randint(MIN, MAX)
+print(targetNumber)
+
+guessedNumber = input("your guess")
+
+if targetNumber = guessedNumber :
+    print("Yay! you guessed correct")
+else :
+    print("Oops! wrong guess")
