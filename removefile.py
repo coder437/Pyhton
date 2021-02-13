c8e50bc57a391842e4e677f721b2e6dcc1c7e421
coder437 <65409756+coder437@users.noreply.github.com>
2021-02-13 11:57:00 +0530

    Update removefile.py

--- removefile.py
+++ removefile.py
@@ -1,1 +1,17 @@
-#work in progress
+import os
+import shutil
+path = input("enter the name of the folder to be sorted")
+list_of_files = os.listdir(path)
+for f in list_of_files:
+    name,ext = os.path.splitext(f)
+    ext = ext[1:]
+    if ext=='':
+        continue
+
+    if os.path.exists(path+"/"+ext):
+        shutil.move(path+"/"+f,path+"/"+ext+"/"+f)
+    else :
+        os.makedirs(path+'/'+ext)
+        shutil.move(path+"/"+f,path+"/"+ext+"/"+f)
+
+    
